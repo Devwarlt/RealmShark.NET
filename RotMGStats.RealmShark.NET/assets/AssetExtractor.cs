f4e40bd7fcc6505fe02f27043e7735fcd0711aa5
Devwarlt <devwarlt@gmail.com>
2025-01-25 18:00:58 -0300

    Create AssetExtractor.cs

---
+++ RotMGStats.RealmShark.NET/assets/AssetExtractor.cs
@@ -0,0 +1,13 @@
+﻿using System.Resources;
+
+namespace RotMGStats.RealmShark.NET.assets
+{
+    /// <summary>
+    /// Main loader for assets. If assets are missing or are outdated, extracts the assets from realm resources files.
+    /// </summary>
+    public class AssetExtractor
+    {
+        public static readonly string ASSETS_OBJECT_FILE_DIR_PATH = "assets/ObjectID.list";
+        public static readonly string ASSETS_TILE_FILE_DIR_PATH = "assets/TileID.list";
+    }
+}
