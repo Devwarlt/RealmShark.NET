9c8a5e41d20bc1f9f11afdd812fa920a077daf24
Devwarlt <devwarlt@gmail.com>
2025-01-26 09:39:04 -0300

    Create WorldPosData.cs

---
+++ RotMGStats.RealmShark.NET/packets/data/WorldPosData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RotMGStats.RealmShark.NET.packets.reader;
+
+namespace RotMGStats.RealmShark.NET.packets.data
+{
+    /// <summary>
+    /// Coordinate data of world objects.
+    /// </summary>
+    [Serializable]
+    public class WorldPosData
+    {
+        /// <summary>
+        /// Position x
+        /// </summary>
+        public float X { get; set; }
+
+        /// <summary>
+        /// Position y
+        /// </summary>
+        public float Y { get; set; }
+
+        /// <summary>
+        /// Deserializer method to extract data from the buffer.
+        /// </summary>
+        /// <param name="buffer">Data that needs deserializing.</param>
+        /// <returns>Returns this object after deserializing.</returns>
+        public WorldPosData Deserialize(BufferReader buffer)
+        {
+            X = buffer.ReadFloat();
+            Y = buffer.ReadFloat();
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return $"WorldPosData (x:{X}, y:{Y})";
+        }
+    }
+}
