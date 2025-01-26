1874c0c72a33d04bf574b05c429ac29939230aa9
Devwarlt <devwarlt@gmail.com>
2025-01-26 09:39:31 -0300

    Create PErrorStop.cs

---
+++ RotMGStats.RealmShark.NET/packets/packetcapture/sniff/assembly/PErrorStop.cs
@@ -0,0 +1,13 @@
+﻿namespace RotMGStats.RealmShark.NET.packets.packetcapture.sniff.assembly
+{
+    /// <summary>
+    /// Class handling large errors stopping the sniffer.
+    /// </summary>
+    public interface PErrorStop
+    {
+        /// <summary>
+        /// Called when a large error is detected attempting to stop sniffing packets.
+        /// </summary>
+        void ErrorStop();
+    }
+}
