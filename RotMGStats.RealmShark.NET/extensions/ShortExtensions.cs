2304c4fd105ded67ae31e01a706e7c47fd89b706
Devwarlt <devwarlt@gmail.com>
2025-01-26 09:38:33 -0300

    Create ShortExtensions.cs

---
+++ RotMGStats.RealmShark.NET/extensions/ShortExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace RotMGStats.RealmShark.NET.extensions
+{
+    public static class ShortExtensions
+    {
+        /// <summary>
+        /// Converts the short to an unsigned integer.
+        /// </summary>
+        /// <param name="value">The short value to convert.</param>
+        /// <returns>The unsigned integer representation of the short.</returns>
+        public static int ToUnsignedInt(this short value)
+        {
+            return value & 0xFFFF;
+        }
+    }
+}
