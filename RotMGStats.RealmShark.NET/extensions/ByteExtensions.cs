2106fe7412fd5e04efe73f923eab65bb5b66d3ec
Devwarlt <devwarlt@gmail.com>
2025-01-26 09:38:26 -0300

    Create ByteExtensions.cs

---
+++ RotMGStats.RealmShark.NET/extensions/ByteExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace RotMGStats.RealmShark.NET.extensions
+{
+    public static class ByteExtensions
+    {
+        /// <summary>
+        /// Converts the byte to an unsigned integer.
+        /// </summary>
+        /// <param name="value">The byte value to convert.</param>
+        /// <returns>The unsigned integer representation of the byte.</returns>
+        public static int ToUnsignedInt(this byte value)
+        {
+            return value & 0xFF;
+        }
+    }
+}
