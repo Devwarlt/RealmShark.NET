19626061a2ff151ec737bdde2d786cebe63652a8
Devwarlt <devwarlt@gmail.com>
2025-01-26 09:39:28 -0300

    Create PErrorMessage.cs

---
+++ RotMGStats.RealmShark.NET/packets/packetcapture/sniff/assembly/PErrorMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace RotMGStats.RealmShark.NET.packets.packetcapture.sniff.assembly
+{
+    /// <summary>
+    /// Called on errors when constructing the TCP stream.
+    /// </summary>
+    public interface PErrorMessage
+    {
+        /// <summary>
+        /// Called when error messages are sent.
+        /// </summary>
+        /// <param name="error">Error message</param>
+        /// <param name="dump">Error dump for debugger</param>
+        void ErrorLogs(string error, string dump);
+    }
+}
