c1e289e7a8745cbe55aebd77841318fbf88e428c
Devwarlt <devwarlt@gmail.com>
2025-01-26 09:38:31 -0300

    Create IntExtensions.cs

---
+++ RotMGStats.RealmShark.NET/extensions/IntExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace RotMGStats.RealmShark.NET.extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Converts the int to an unsigned long.
+        /// </summary>
+        /// <param name="value">The int value to convert.</param>
+        /// <returns>The unsigned long representation of the int.</returns>
+        public static ulong ToUnsignedLong(this int value)
+        {
+            return (ulong)(uint)value;
+        }
+    }
+}
