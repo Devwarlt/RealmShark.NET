7ebebc1a4c8228970556856c92577bdde02ae723
Devwarlt <devwarlt@gmail.com>
2025-01-26 09:39:34 -0300

    Create PReset.cs

---
+++ RotMGStats.RealmShark.NET/packets/packetcapture/sniff/assembly/PReset.cs
@@ -0,0 +1,14 @@
+﻿namespace RotMGStats.RealmShark.NET.packets.packetcapture.sniff.assembly
+{
+    /// <summary>
+    /// Reset interface used in stream constructor for sending reset updates when
+    /// receiving reset packets.
+    /// </summary>
+    public interface PReset
+    {
+        /// <summary>
+        /// Method called when a reset packet is received.
+        /// </summary>
+        void Reset();
+    }
+}
