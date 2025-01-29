30ecb6dc3a5c8d52b573a77de8e869dda5755ffa
Devwarlt <devwarlt@gmail.com>
2025-01-29 14:52:57 -0300

    Create IPacketListener.cs

---
+++ packets/packetcapture/register/IPacketListener.cs
@@ -0,0 +1,13 @@
+﻿using RotMGStats.RealmShark.NET.packets;
+
+namespace RotMGStats.RealmShark.NET.packets.packetcapture.register
+{
+    /// <summary>
+    /// Listener interface used in the registry class matching subscribed classes to packet classes.
+    /// </summary>
+    /// <typeparam name="T">The type of packet.</typeparam>
+    public interface IPacketListener<T> where T : Packet
+    {
+        void Process(T packet);
+    }
+}
